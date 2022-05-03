4dc48f86fd605831dba4742a44635761ed769db4
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-05-03 14:59:37 -0700

    started form event listener

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const form = document.getElementById('add-ingredients');
 let ingredients = [];
 
 // set event listeners 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+const data = new FormData(form);
+});
+
+
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
